8c1ddc3bce14af9acd30b86b965b4ef5211b9827
120267583@qq.com <120267583@qq.com>
2016-10-28 09:51:07 +0800

    拆分文件

---
+++ inc/doMobileIndex.php
@@ -0,0 +1,35 @@
+<?php
+
+global $_W;
+//这个操作被定义用来呈现 功能封面
+$openid=$_W['openid'];
+
+//var_dump($openid);
+
+$weid=$_W['uniacid'];
+$timestamp=$_W['timestamp'];
+
+$result=pdo_getall('wx_bookcar_drivers',[
+    'weid'=>$weid,
+    'open_id'=>$openid,
+]);
+
+if(count($result)<1){
+    $result = pdo_insert('wx_bookcar_drivers', [
+        'weid'=>$weid,
+        'open_id'=>$openid,
+        'create_time'=> date('Y-m-d H:i:s', $timestamp)
+    ]);
+}
+
+
+
+
+//var_dump($result);
+
+
+//var_dump(tablename('wx_bookcar_drivers'));
+
+
+include $this->template('index');
+?>
--- site.php
+++ site.php
@@ -10,57 +10,27 @@ defined('IN_IA') or exit('Access Denied');
 class Wx_bookcarModuleSite extends WeModuleSite {
 
     function __construct(){
-       global $_W;
-       // if(!$_W['openid'])die();
+        global $_W;
+        // if(!$_W['openid'])die();
     }
 
-	public function doMobileIndex() {
-	    global $_W;
-		//这个操作被定义用来呈现 功能封面
-        $openid=$_W['openid'];
-        
-        //var_dump($openid);
-        
-        $weid=$_W['uniacid'];
-        $timestamp=$_W['timestamp'];
-        
-        $result=pdo_getall('wx_bookcar_drivers',[
-                'weid'=>$weid,
-                'open_id'=>$openid,
-            ]);
-            
-        if(count($result)<1){
-            $result = pdo_insert('wx_bookcar_drivers', [
-            'weid'=>$weid,
-            'open_id'=>$openid,
-            'create_time'=> date('Y-m-d H:i:s', $timestamp)
-            ]);
-        }
-        
-       
+    public function doMobileIndex() {
 
-            
-        //var_dump($result);
-
-
-//var_dump(tablename('wx_bookcar_drivers'));
-        
-
-        include $this->template('index');
-	}
+        require_once(dirname(__FILE__)."/inc/doMobileIndex.php");
+    }
     public function doMobileDriver() {
         //这个操作被定义用来呈现 功能封面
         include $this->template('driver');
     }
 
-	public function doWebOrder() {
-	    
-	    var_dump(123);
-		//这个操作被定义用来呈现 管理中心导航菜单
-	}
-	public function doMobilePersonal() {
-		//这个操作被定义用来呈现 微站个人中心导航
-	}
+    public function doWebOrder() {
+
+        var_dump(123);
+        //这个操作被定义用来呈现 管理中心导航菜单
+    }
+    public function doMobilePersonal() {
+        //这个操作被定义用来呈现 微站个人中心导航
+    }
 
 
 
