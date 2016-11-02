c389c37bca5525daeabfee944170944f75d52e99
120267583@qq.com <120267583@qq.com>
2016-11-02 15:15:50 +0800

    定位

--- inc/doMobileAjaxDriverAsync.php
+++ inc/doMobileAjaxDriverAsync.php
@@ -11,17 +11,23 @@ $weid=$_W['uniacid'];  /*公众号id*/
 $timestamp=$_W['timestamp'];
 $time=date('Y-m-d H:i:s', $timestamp);
 
-$data= pdo_get('wx_bookcar_drivers', array('weid'=>$weid,'openid' => $openid)); /*获取当前司机信息*/
+$data= pdo_get('wx_bookcar_drivers', array('weid'=>$weid,'open_id' => $openid)); /*获取当前司机信息*/
 if(count($data)>1){
     /*判断在司机表里面是否存在*/
     $condition=[
         'weid'=>$weid,
-        'openid' => $openid
+        'open_id' => $openid
     ];
     $updateData=[
         'longitude'=>$longitude,
         'latitude'=>$latitude,
         'update_time'=>$time
     ];
-    $result = pdo_update('wx_bookcar_drivers',$data, $condition);
+    $result = pdo_update('wx_bookcar_drivers',$updateData, $condition);
+
+    echo json_encode(["ret"=>0,'other'=>$updateData]);
+
+}else{
+    echo json_encode(["ret"=>1]);
+
 }
--- inc/doMobileTest.php
+++ inc/doMobileTest.php
@@ -1,2 +1,3 @@
 <?php
+global $_W,$_GPC;
 include $this->template('test');
