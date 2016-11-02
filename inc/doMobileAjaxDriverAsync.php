916aee71361c8f616e283773f0bb7e391472cf0d
120267583@qq.com <120267583@qq.com>
2016-11-02 14:29:14 +0800

    添加司机同步位置接口

--- inc/doMobileAjaxDriverAsync.php
+++ inc/doMobileAjaxDriverAsync.php
@@ -1,1 +1,27 @@
 <?php
+global $_W,$_GPC;
+
+$longitude= floatval($_GPC['longitude']); /*经度*/
+$latitude= floatval($_GPC['latitude']);  /*纬度*/
+
+$openid=$_W['openid'];  /*用户openid*/
+$weid=$_W['uniacid'];  /*公众号id*/
+
+
+$timestamp=$_W['timestamp'];
+$time=date('Y-m-d H:i:s', $timestamp);
+
+$data= pdo_get('wx_bookcar_drivers', array('weid'=>$weid,'openid' => $openid)); /*获取当前司机信息*/
+if(count($data)>1){
+    /*判断在司机表里面是否存在*/
+    $condition=[
+        'weid'=>$weid,
+        'openid' => $openid
+    ];
+    $updateData=[
+        'longitude'=>$longitude,
+        'latitude'=>$latitude,
+        'update_time'=>$time
+    ];
+    $result = pdo_update('wx_bookcar_drivers',$data, $condition);
+}
